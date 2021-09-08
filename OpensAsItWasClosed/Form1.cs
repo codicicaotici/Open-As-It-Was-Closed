7ffad663d8b8c524183c085461dfed96870df684
codicicaotici <70903864+codicicaotici@users.noreply.github.com>
2021-09-08 18:26:21 +0200

    Add files via upload

---
+++ OpensAsItWasClosed/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace OpensAsItWasClosed
+{
+    public partial class Form1 : Form
+    {
+        ini myIni = new ini();
+        public string  CurrentDirectory = Directory.GetCurrentDirectory();
+
+        public Form1()
+        {
+            InitializeComponent();
+            //leggi le impostazioni in entrata
+            string iniFile = CurrentDirectory + "form1.ini";
+            string settingTitle = "main_setting";
+            if (myIni.getIniValue(iniFile, settingTitle, "FormWindowState", "") == "Maximized") { this.WindowState = System.Windows.Forms.FormWindowState.Maximized; }
+            if (myIni.getIniValue(iniFile, settingTitle, "FormWindowState", "") == "Minimized") { this.WindowState = System.Windows.Forms.FormWindowState.Minimized; }
+            if (myIni.getIniValue(iniFile, settingTitle, "FormWindowState", "") == "Normal")
+            {   
+                this.Left = Int32.Parse(myIni.getIniValue(iniFile, settingTitle, "MainLeft", ""));
+                this.Top = Int32.Parse(myIni.getIniValue(iniFile, settingTitle, "MainTop", ""));
+                this.Width = Int32.Parse(myIni.getIniValue(iniFile, settingTitle, "MainWidth", ""));
+                this.Height = Int32.Parse(myIni.getIniValue(iniFile, settingTitle, "MainHeight", ""));
+                this.StartPosition = FormStartPosition.Manual;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Salva le impostazioni in uscita
+            string iniFile = CurrentDirectory + "form1.ini";
+            string settingTitle = "main_setting";
+            string winstate = "";
+            if (this.WindowState == System.Windows.Forms.FormWindowState.Maximized)
+            {
+                winstate = "Maximized";
+                myIni.setIniValue(iniFile, settingTitle, "FormWindowState", winstate);
+            }
+            if (this.WindowState == System.Windows.Forms.FormWindowState.Minimized)
+            {
+                winstate = "Minimized";
+                myIni.setIniValue(iniFile, settingTitle, "FormWindowState", winstate);
+            }
+            if (this.WindowState == System.Windows.Forms.FormWindowState.Normal)
+            {
+                winstate = "Normal";
+                myIni.setIniValue(iniFile, settingTitle, "FormWindowState", winstate);
+                myIni.setIniValue(iniFile, settingTitle, "MainLeft", this.Left.ToString());
+                myIni.setIniValue(iniFile, settingTitle, "MainTop", this.Top.ToString());
+                myIni.setIniValue(iniFile, settingTitle, "MainWidth", this.Width.ToString());
+                myIni.setIniValue(iniFile, settingTitle, "MainHeight", this.Height.ToString());
+            }
+            Close();
+        }
+
+        /* ****************************************** */
+
+    }//public partial class Form1 : Form
+}//namespace OpensAsItWasClosed
---
+++ OpensAsItWasClosed/ini.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+
+namespace OpensAsItWasClosed
+{
+    class ini
+    {
+        //INI file ********
+        [DllImport("KERNEL32.DLL", EntryPoint = "WritePrivateProfileStringW",
+            SetLastError = true,
+            CharSet = CharSet.Unicode, ExactSpelling = true,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern int WritePrivateProfileString
+            (
+            string lpAppName,
+            string lpKeyName,
+            string lpString,
+            string lpFilename
+            );
+        [DllImport("KERNEL32.DLL", EntryPoint = "GetPrivateProfileStringW",
+            SetLastError = true,
+            CharSet = CharSet.Unicode, ExactSpelling = true,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern int GetPrivateProfileString
+            (
+            string lpAppName,
+            string lpKeyName,
+            string lpDefault,
+            string lpReturnString,
+            int nSize,
+            string lpFilename
+            );
+
+        /* code *************************************** */
+        public void setIniValue(string iniFile, string category, string key, string value)
+        {
+            WritePrivateProfileString(category, key, value, iniFile);
+        }
+        /// <summary>
+        /// Gets the keys.
+        /// </summary>
+        /// <param name="iniFile">The ini file.</param>
+        /// <param name="category">The category.</param>
+        public string getIniValue(string iniFile, string category, string key, string defaultValue)
+        {
+            string returnString = new string(' ', 65536);
+            GetPrivateProfileString(category, key, defaultValue, returnString, 65536, iniFile);
+            return returnString.Split('\0')[0];
+        }/// 
+        /* ****************************************** */
+    }//class ini
+}//namespace OpensAsItWasClosed
